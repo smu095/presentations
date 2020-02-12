d281730b2c5350f311813463317b3eb6377413de
seamurra <sean.murray@inmeta.no>
2020-02-12 13:49:42 +0100

    removed counts from script

--- mcmc-pymc3-101/marbles.py
+++ mcmc-pymc3-101/marbles.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+## Observed data
+data = np.array([1, 0, 1])
+
 ## Conjectures (possible values for theta)
 # array([0., 0.25, 0.5, 0.75, 1.])
 theta = np.linspace(0, 1, num=5)
@@ -9,17 +12,15 @@ theta = np.linspace(0, 1, num=5)
 uniform_prior = np.repeat(0.2, repeats=5)
 
 # Likelihood function
-likelihood = lambda p: p**2 * (1 - p)
+def likelihood(data, theta):
+    num_blue = data.sum()
+    num_white = len(data) - num_blue
+    return theta**num_blue * (1 - theta)**num_white
 
 # Unnormalised posterior over theta
 # array([0., 0.009375, 0.025, 0.028125, 0.])
-unnormalised_posterior = uniform_prior * likelihood(theta) 
+unnormalised_posterior = uniform_prior * likelihood(data, theta) 
 
 # Posterior distribution over theta
 # array([0., 0.15, 0.4, 0.45, 0.])
 posterior = unnormalised_posterior / unnormalised_posterior.sum()
-
-# McElreath's possible marble counts
-forking_data_counts = np.array([0, 3, 8, 9, 0])
-forking_data_counts / forking_data_counts.sum()
-# array([0., 0.15, 0.4, 0.45, 0.])
