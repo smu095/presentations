1a1c3bf853e4b1e99d8391f22f86b45f4da9dc5b
Solveig Masvie <solveig.masvie@inmeta.no>
2020-02-17 08:08:32 +0100

    owl example + MCMC illustration

--- mcmc-pymc3-101/code/metropolis.py
+++ mcmc-pymc3-101/code/metropolis.py
@@ -1,27 +1,25 @@
 """Metropolis sampling from posterior of mean"""
-
 import numpy as np
+data = np.array([1, 0, 1]) # Observations
 
-# N = 10 observations from Normal(loc=5, scale=2)
-unknown_mean, known_variance = 5, 2
-data = true_mean + np.sqrt(known_variance) * np.random.randn(10)
+def log_p(theta, x): # Proportional log posterior
+    N, k = len(x), np.sum(x)
+    logprior = np.repeat(0.2, N)
+    loglik = k*np.log(theta + 10e-8) + (N - k)*np.log(1 - theta + 10e-8)
+    return np.sum(logprior + loglik)
 
-def loglik(theta, x):
-    """Log-likelihood of Normal distribution."""
-    return np.sum(st.norm(loc=theta, scale=known_variance).logpdf(x))
-
-# Initial value for theta
-theta = 0
+theta = 0.5 # Initial value for theta
 num_samples = 10**4
 samples = np.zeros(num_samples)
 
 # Metropolis algorithm
 for i in range(num_samples):
     # Draw proposal
-    theta_star = theta + np.random.randn(1)
+    theta_star = theta + np.sqrt(0.05)*np.random.randn(1)
+    theta_star = max(0, min(theta_star, 1))
     
     # Accept/reject
     u = np.random.rand()
-    if u < np.exp(loglik(theta_star, data) - loglik(theta, data)):
+    if u < np.exp(log_p(theta_star, data) - log_p(theta, data)):
         theta = theta_star
     samples[i] = theta
